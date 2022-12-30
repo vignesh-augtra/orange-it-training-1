6b7da2d01a9b48892285b7978044353ea4385ce7
vignesh-augtra <vignesh.augtra@gmail.com>
2022-12-30 13:15:11 +0530

    added form

---
+++ Materialize CSS/form/controller.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    $(".sidenav").sidenav({
+        edge:"left"
+    })
+    $(".datepicker").datepicker()
+
+
+    $("select").formSelect()
+
+
+
+})
